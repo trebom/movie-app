7db5f157b447ec57c68cd192f354e39faf3b7661
Your Name Here <shinykim63@gmail.com>
2020-10-06 20:47:01 -0400

    Updates

---
+++ precache-manifest.0b75d95b62c1c27dba824552255d3774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb3609eb91591a27dd49e43000670fc",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "64df9b5d9e42d12d318e",
+    "url": "/movie-app/static/css/main.52e8d811.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64df9b5d9e42d12d318e",
+    "url": "/movie-app/static/js/main.230f4563.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
